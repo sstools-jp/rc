6c32537341bde9733180d6db322f0688378b8f89
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-06-10 14:48:32 +0900

    feat: 算出式の順番を整理

--- src/data/resultTooltips.ts
+++ src/data/resultTooltips.ts
@@ -57,10 +57,10 @@ export const resultTooltips: Record<string, TooltipContent> = {
     nestedLines: [
       {
         line: math`f(\theta) =
-               \begin{cases}
-               f_{\text{zero}}   (\theta, \theta_{\text{in}}) & (N = 0) \\
-               f_{\text{nonzero}}(\theta, \theta_{\text{in}}) & (N \ne 0)
-               \end{cases}`,
+                    \begin{cases}
+                    f_{\text{zero}}   (\theta, \theta_{\text{in}}) & (N = 0) \\
+                    f_{\text{nonzero}}(\theta, \theta_{\text{in}}) & (N \ne 0)
+                    \end{cases}`,
         nestedLines: [
           math`\begin{aligned}
                f_{\text{zero}}(\theta, \theta_{\text{in}}) = &
@@ -103,63 +103,54 @@ export const resultTooltips: Record<string, TooltipContent> = {
   f_c: {
     title: "コンクリート圧縮応力度係数",
     line: math`\begin{aligned}
-           f_c(\theta, \theta_{\text{in}}) =
-           & \frac{\text{Numerator}}{\text{Denominator}} \\
-           \text{Numerator} = & 1 - \cos\theta \\
-           \text{Denominator} =
-           & \frac{2}{3} \sin^3\theta - \theta \cos\theta + \sin\theta \cos^2\theta + \frac{\theta}{4}
-           \ - \frac{1}{4} \sin\theta \cos\theta - \frac{1}{6} \sin^3\theta \cos\theta \\
-           & - \gamma^3 \left[ \frac{2}{3} \sin^3\theta_{\text{in}} - \theta_{\text{in}} \cos\theta_{\text{in}}
-           \ + \sin\theta_{\text{in}} \cos^2\theta_{\text{in}} \\
-           \ + \gamma \left( \frac{\theta_{\text{in}}}{4} - \frac{1}{4} \sin\theta_{\text{in}} \cos\theta_{\text{in}}
-           \ - \frac{1}{6} \sin^3\theta_{\text{in}} \cos\theta_{\text{in}} \right) \right] \\
-           & + \pi n p \left( \frac{1}{2} \alpha^2 - \cos\theta \right)
-           \end{aligned}`,
+                f_c(\theta, \theta_{\text{in}}) =
+                & \frac{\text{Numerator}}{\text{Denominator}} \\
+                \text{Numerator} = & 1 - \cos\theta \\
+                \text{Denominator} =
+                & \frac{2}{3} \sin^3\theta - \theta \cos\theta + \sin\theta \cos^2\theta + \frac{\theta}{4}
+                \ - \frac{1}{4} \sin\theta \cos\theta - \frac{1}{6} \sin^3\theta \cos\theta \\
+                & - \gamma^3 \left[ \frac{2}{3} \sin^3\theta_{\text{in}} - \theta_{\text{in}} \cos\theta_{\text{in}}
+                \ + \sin\theta_{\text{in}} \cos^2\theta_{\text{in}} \\
+                \ + \gamma \left( \frac{\theta_{\text{in}}}{4} - \frac{1}{4} \sin\theta_{\text{in}} \cos\theta_{\text{in}}
+                \ - \frac{1}{6} \sin^3\theta_{\text{in}} \cos\theta_{\text{in}} \right) \right] \\
+                & + \pi n p \left( \frac{1}{2} \alpha^2 - \cos\theta \right)
+                \end{aligned}`,
   },
   f_s: {
     title: "鋼材応力度係数",
-    line: math`f_s(\theta, \theta_{\text{in}})
-           = f_c(\theta, \theta_{\text{in}}) \times \dfrac{\alpha + \cos\theta}{1 - \cos\theta}`,
+    line: math`f_s(\theta, \theta_{\text{in}}) = f_c(\theta, \theta_{\text{in}}) \times \dfrac{\alpha + \cos\theta}{1 - \cos\theta}`,
   },
   f_v: {
     title: "せん断応力度係数",
     line: math`f_v(\theta, \theta_{\text{in}}) = \dfrac
-                   {
-                     \alpha \sin x_s - (\pi - x_s)(z_3 - 1)
-                   }{
-                     2 z_2 ( \sin\theta - \gamma \sin\theta_{\text{in}} )
-                   } n p`,
+                {
+                  \alpha \sin x_s - (\pi - x_s)(z_3 - 1)
+                }{
+                  2 z_2 ( \sin\theta - \gamma \sin\theta_{\text{in}} )
+                } n p`,
     nestedLines: [
-      {
-        line: math`x_s = \arccos\left( \dfrac{1}{\alpha} \cos\theta \right)`,
-        nestedLines: [
-          {
-            line: math`\begin{array}{rcl}
-               z_2 & = & z_3^2 (\theta - \sin\theta \cos\theta)
-                       \ + 2 z_3 \left( -\theta + \sin\theta \cos\theta + \dfrac{2}{3} \sin^3\theta \right) \\
-                   &   & + \dfrac{5}{4} ( \theta - \sin\theta \cos\theta )
-                         + \sin^3\theta \left( \dfrac{\cos\theta}{2} - \dfrac{4}{3} \right) \\
-                   &   & - \gamma^2 z_3^2 ( \theta_{\text{in}} - \sin\theta_{\text{in}} \cos\theta_{\text{in}} )
-                       \ - 2 \gamma^2 z_3 \left( -\theta_{\text{in}} + \sin\theta_{\text{in}} \cos\theta_{\text{in}} +
-                         \dfrac{2}{3} \gamma \sin^3\theta_{\text{in}} \right) \\
-                   &   & - \gamma^2 \left( 1 + \dfrac{\gamma^2}{4} \right) ( \theta_{\text{in}} - \sin\theta_{\text{in}} \cos\theta_{\text{in}} )
-                       \ - \gamma^3 \sin^3\theta_{\text{in}} \left( \dfrac{\gamma \cos\theta_{\text{in}}}{2} - \dfrac{4}{3} \right) \\
-                   &   & + \pi n p \left(\dfrac{\alpha^2}{2} + (1 - z_3)^2 \right)
+      math`x_s = \arccos\left( \dfrac{1}{\alpha} \cos\theta \right)`,
+      math`\begin{array}{rcl}
+            z_2 & = & z_3^2 (\theta - \sin\theta \cos\theta)
+                    \ + 2 z_3 \left( -\theta + \sin\theta \cos\theta + \dfrac{2}{3} \sin^3\theta \right) \\
+                &   & + \dfrac{5}{4} ( \theta - \sin\theta \cos\theta )
+                      + \sin^3\theta \left( \dfrac{\cos\theta}{2} - \dfrac{4}{3} \right) \\
+                &   & - \gamma^2 z_3^2 ( \theta_{\text{in}} - \sin\theta_{\text{in}} \cos\theta_{\text{in}} )
+                    \ - 2 \gamma^2 z_3 \left( -\theta_{\text{in}} + \sin\theta_{\text{in}} \cos\theta_{\text{in}} +
+                      \dfrac{2}{3} \gamma \sin^3\theta_{\text{in}} \right) \\
+                &   & - \gamma^2 \left( 1 + \dfrac{\gamma^2}{4} \right) ( \theta_{\text{in}} - \sin\theta_{\text{in}} \cos\theta_{\text{in}} )
+                    \ - \gamma^3 \sin^3\theta_{\text{in}} \left( \dfrac{\gamma \cos\theta_{\text{in}}}{2} - \dfrac{4}{3} \right) \\
+                &   & + \pi n p \left(\dfrac{\alpha^2}{2} + (1 - z_3)^2 \right)
                \end{array}`,
-            nestedLines: [
-              math`z_3 = \dfrac
-               {
-                 \theta - \sin\theta \cos\theta - \dfrac{2}{3} \sin^3\theta -
-                 \gamma^2 \left( \theta_{\text{in}} - \sin\theta_{\text{in}} \cos\theta_{\text{in}} -
-                 \dfrac{2}{3} \gamma \sin^3\theta_{\text{in}} \right) + \pi n p
-               }{
-                 \theta - \sin\theta \cos\theta - \gamma^2 ( \theta_{\text{in}} -
-                 \sin\theta_{\text{in}} \cos\theta_{\text{in}} ) + \pi n p
-               }`,
-            ],
-          },
-        ],
-      },
+      math`z_3 = \dfrac
+            {
+              \theta - \sin\theta \cos\theta - \dfrac{2}{3} \sin^3\theta -
+              \gamma^2 \left( \theta_{\text{in}} - \sin\theta_{\text{in}} \cos\theta_{\text{in}} -
+              \dfrac{2}{3} \gamma \sin^3\theta_{\text{in}} \right) + \pi n p
+            }{
+              \theta - \sin\theta \cos\theta - \gamma^2 ( \theta_{\text{in}} -
+              \sin\theta_{\text{in}} \cos\theta_{\text{in}} ) + \pi n p
+            }`,
     ],
   },
 };
